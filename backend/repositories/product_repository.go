1731111143a1d7ac249fa73d7005fc311d7e09ff
Upistudio <luthfiluthfi355@gmail.com>
2026-07-09 03:25:14 +0700

    feat: menyiapkan crud dan postman testing

--- backend/repositories/product_repository.go
+++ backend/repositories/product_repository.go
@@ -13,3 +13,11 @@ func GetAllProducts() ([]models.Product, error) {
 func SaveProduct(product *models.Product) error {
 	return config.DB.Create(product).Error
 }
+
+func UpdateProduct(product *models.Product) error {
+	return config.DB.Save(product).Error
+}
+
+func DeleteProduct(id uint) error {
+	return config.DB.Delete(&models.Product{}, id).Error
+}
